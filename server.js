a87d7ffb28550c2333ebdd47ffe0e06500f313bb
PRINCE <anshu84475@gmail.com>
2025-12-31 17:48:30 +0530

    fix port for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -32,3 +32,4 @@ app.get("/orders", (req, res) => {
 app.listen(PORT, () => {
   console.log("Backend running on port " + PORT);
 });
+
